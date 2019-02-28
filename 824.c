c2de7bfeb9a8607ded466310cc1df59fb095d676
AkaShark <1548742234@qq.com>
2019-02-28 13:04:25 +0800

    添加LeetCode 解释

--- 824.c
+++ 824.c
@@ -74,3 +74,45 @@ char* toGoatLatin(char* S)
     return fun;
 }
 
+//LeetCode答案 其实就是创建了新的字符数组然后将原先的字符数组通过规则组装成新的数组
+// 然后将新的数组添加 “ ”等之类的 完成数组的组装 完成后就返回就好了
+char a[10000];
+char* toGoatLatin(char* S) {
+    memset(a,0,sizeof(a));
+    char *s=S,p;
+    int i=0,count=0,flag=1,flag2=0,t=1;
+    while(s[i]!=0) // 大循环循环全部的字符
+    {
+//        一个单词从S到a的转化
+        t++;
+        while(s[i]!=' '&&s[i]!=0) // 找出一个词
+        {
+            if(flag) // 第一个字母
+            {
+                if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+                {
+                    a[count++]=s[i]; // 元音情况
+                }
+                else // 辅音情况
+                {
+                    flag2=1;
+                    p=s[i];
+                }
+                flag=0;
+            }else
+            {
+                a[count++]=s[i];
+            }
+            i++;
+        }
+        if(flag2) a[count++]=p; // 辅音将首字母放在后面
+        a[count++]='m'; // 添加m
+        for(int i=0;i<t;i++)   a[count++]='a'; // 添加对应个数的a
+        if(s[i]==0) break;
+        a[count++]=' '; // 补充a的空格
+        i++;
+        flag2=0;
+        flag=1;
+    }
+    return a;
+}
